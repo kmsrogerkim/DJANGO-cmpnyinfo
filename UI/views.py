62c3ac401f291f72f97eb8e690ddf6e8d1b78992
samsunglaptop <minseungkim1017@gmail.com>
2024-01-11 10:58:26 +0000

    HOT FIX: Removed (temp) sign-in/up feature

--- UI/views.py
+++ UI/views.py
@@ -8,38 +8,36 @@ def home(request):
         if request.POST.get("cmpnyname"):
             cmpnyname = request.POST.get("cmpnyname")
             return redirect("cmpny", cmpnyname=cmpnyname)
-        else:
-            return redirect("sign_up")
     return render(request, "home.html")
 
-def sign_up(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            usernamev = request.POST.get('username')
-            passwordv = request.POST.get('password2')
-            user = authenticate(username = usernamev, password = passwordv)
-            login(request, user)
-            return redirect("home")
-    else:
-        form = RegisterForm()
-        return render(request, "sign_up.html", {"form":form})
-    return render(request, "sign_up.html", {"form":form})
+# def sign_up(request):
+#     if request.method == 'POST':
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             usernamev = request.POST.get('username')
+#             passwordv = request.POST.get('password2')
+#             user = authenticate(username = usernamev, password = passwordv)
+#             login(request, user)
+#             return redirect("home")
+#     else:
+#         form = RegisterForm()
+#         return render(request, "sign_up.html", {"form":form})
+#     return render(request, "sign_up.html", {"form":form})
 
-def sign_in(request):
-    if request.method == 'POST':
-        usernamev = request.POST.get('username')
-        passwordv = request.POST.get('password')
-        user = authenticate(request, username=usernamev, password = passwordv)
-        if user is not None:
-            login(request, user)
-            return redirect("home")
-        else:
-            messages.success(request, "Incorrect password/username. Please TRY AGAIN!")
-            return redirect("sign_in")
-    else:
-        return render(request, "sign_in.html")
+# def sign_in(request):
+#     if request.method == 'POST':
+#         usernamev = request.POST.get('username')
+#         passwordv = request.POST.get('password')
+#         user = authenticate(request, username=usernamev, password = passwordv)
+#         if user is not None:
+#             login(request, user)
+#             return redirect("home")
+#         else:
+#             messages.success(request, "Incorrect password/username. Please TRY AGAIN!")
+#             return redirect("sign_in")
+#     else:
+#         return render(request, "sign_in.html")
 
 def cmpny(request, cmpnyname):
     return render(request, "cmpny.html", {"cmpnyname":cmpnyname})
