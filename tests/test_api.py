ab3d71576e158952f9de244a9c45021626a9ba03
RogerKimJazzLover <minseungkim1017@gmail.com>
2024-02-14 06:13:09 +0900

    test with django.test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,17 @@
-import pytest
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory #module for mocking requests
 
 from API import views
 
 class Request():
-    def __init__(self) -> None:
-        self.data = {"cmpnyname" : "삼성전자"}
+    def __init__(self, url: str, data: dict) -> None:
+        self.factory = APIRequestFactory()
+        # self.url = url
+        # self.data = data
+        self.request = self.factory.post(url, data)
+class APITest(TestCase):
+    def setUp(self):
+        self.reqeust = Request("api/basicInfo/", {"cmpnyname" : "삼성전자"}).request
 
-def test_get_basic_info():
-    reqeust = Request()
-    API.views.get_basic_info(reqeust)
+    def test_get_basic_info(self):
+        views.get_basic_info(self.reqeust)
