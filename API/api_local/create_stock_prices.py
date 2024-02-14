e88969b143693fc2bf912b7878741ced31f97030
RogerKimJazzLover <minseungkim1017@gmail.com>
2024-02-14 17:14:13 +0900

    FIX: create_stock_prices to have dates

--- API/api_local/create_stock_prices.py
+++ API/api_local/create_stock_prices.py
@@ -12,7 +12,7 @@ import logging
 def CreateSaveDF(cmpnyname: str, corp_code: str, year: str):
     sp_data = fdr.DataReader(corp_code, "2017-12-01", f"{year}-12-31") #GETTING A DATAFRAME OF THE STOCKPRICES OF THE CORP_CODE CORPORATION
     save_file_path = os.path.join(os.getcwd(), 'API', 'api_local', 'Data', 'Stocks', f"{cmpnyname}_stock.csv")
-    sp_data.to_csv(save_file_path, encoding='euc-kr', index=False)
+    sp_data.to_csv(save_file_path, encoding='euc-kr', index=True)
 
 def main():
     #GETTING THE LIST OF NAMES AND THEIR CORPORATE CODES IN THE KOSPI FROM THE pkl object make with the modul in the Data directory
