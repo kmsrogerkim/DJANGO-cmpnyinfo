0be4f9aaee79db6b55eeb2931408b872bff0f1a8
RogerKimJazzLover <minseungkim1017@gmail.com>
2024-02-21 17:44:19 +0900

    UPDATE: test_api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,26 +1,25 @@
 from rest_framework.test import APIRequestFactory #module for mocking requests
-import pytest, json
 import numpy as np
-import pprint
+import pytest
 
 from API import views
 
 class Request():
-    def __init__(self, url: str, data: dict) -> None:
+    def __init__(self, url: str) -> None:
         self.factory = APIRequestFactory()
-        self.request = self.factory.post(url, data)
+        self.request = self.factory.get(url)
 
 class TestByCmpnyName():
     def setup_method(self, method):
         print(f"Setting up {method}")
-        self.good_request_data = {"cmpnyname" : "삼성전자"}
-        self.young_request_data = {"cmpnyname" : "삼성전자우"}
-        self.not_found_request_data = {"cmpnyname" : "김민승"}
+        self.good_cmpnyData = "삼성전자"
+        self.young_cmpnyData = "삼성전자우"
+        self.not_found_cmpnyData = "김민승"
 
     def test_get_basic_info(self):
-        request = Request("api/basicInfo/", self.good_request_data).request
+        request = Request("api/basicInfo").request
 
-        response = views.get_basic_info(request)
+        response = views.get_basic_info(request, self.good_cmpnyData)
         response = response.data
 
         assert response["cmpnyname"] == "삼성전자" and len(list(response.keys())) == 7 and isinstance(response["Yesterday"], np.int64)
@@ -29,39 +28,39 @@ class TestByCmpnyName():
         '''
         The api must return 404 when the cmpnyname does not exist
         '''
-        request = Request("api/basicInfo/", self.not_found_request_data).request
-        response = views.get_basic_info(request)
+        request = Request("api/basicInfo").request
+        response = views.get_basic_info(request, self.not_found_cmpnyData)
         assert response.status_code == 404
 
     def test_get_finstate_sum(self):
-        request = Request("api/finstateSum/", self.good_request_data).request
+        request = Request("api/finstateSum").request
 
-        response = views.get_finstate_sum(request)
+        response = views.get_finstate_sum(request, self.good_cmpnyData)
         response = response.data
 
         assert len(response) == 6 and len(response[0].keys()) == 16
 
     def test_fail_get_finstate_sum(self):
-        request = Request("api/finstateSum/", self.young_request_data).request
-        response = views.get_finstate_sum(request)
+        request = Request("api/finstateSum").request
+        response = views.get_finstate_sum(request, self.young_cmpnyData)
         assert response.status_code == 400
 
     def test_get_graph_data(self):
-        request = Request("api/graphData/", self.good_request_data).request
+        request = Request("api/graphData/").request
 
-        response = views.get_graph_data(request)
+        response = views.get_graph_data(request, self.good_cmpnyData)
         response = response.data
 
         assert len(response.keys()) == 3
 
     def test_fail_get_graph_data(self):
-        request = Request("api/graphData/", self.young_request_data).request
-        response = views.get_graph_data(request)
+        request = Request("api/graphData").request
+        response = views.get_graph_data(request, self.young_cmpnyData)
         assert response.status_code == 400
 
 class TestByCorpCode(TestByCmpnyName):
     def setup_method(self, method):
         print(f"Setting up {method}")
-        self.good_request_data = {"cmpnyname" : "005930"}
-        self.young_request_data = {"cmpnyname" : "005935"}
-        self.not_found_request_data = {"cmpnyname" : "0"}
+        self.good_cmpnyData = "005930"
+        self.young_cmpnyData = "005935"
+        self.not_found_cmpnyData = "0"
