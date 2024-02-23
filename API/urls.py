5fb5336d48aba993a0960cd275b362b1c6709668
RogerKimJazzLover <minseungkim1017@gmail.com>
2024-02-23 17:09:04 +0900

    HOT FIX: about page url

--- API/urls.py
+++ API/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
 	path("basicInfo/<str:cmpnyData>", views.get_basic_info),
 	path("finstateSum/<str:cmpnyData>", views.get_finstate_sum),
 	path("graphData/<str:cmpnyData>", views.get_graph_data),
-    path("cmpnyList/<str:cmpnyData>", views.get_cmpny_list)
+    path("cmpnyList", views.get_cmpny_list)
 ]
