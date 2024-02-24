f464a153e2178d3406e06d297ae28543eee36e53
RogerKimJazzLover <minseungkim1017@gmail.com>
2024-02-25 01:51:45 +0900

    NEW: added gunicorn with docker-compose

--- cmpnyinfo/settings.py
+++ cmpnyinfo/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w5o5*72v&cy%r(b35+ai6+v-(f%=t3go1oc3vja&7n6*plm2us'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = "static/"
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
